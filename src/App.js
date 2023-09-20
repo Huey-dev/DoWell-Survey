764387c1345f24734ee561a2026e42689a213628
Tewodros Abebe <teddyab729@gmail.com>
2023-09-20 11:38:50 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import Screen4 from "./Components/Screen4/Screen4";
 import Form from "./Components/Screen4/Form";
 

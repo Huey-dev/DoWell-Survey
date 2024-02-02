d0c8ddeb13c3efc6c8e339865abc07fc8e61ca93
unknown <ericmbuthia11@gmail.com>
2024-02-02 17:55:29 +0300

    Vite config edition

--- dowell-survey/vite.config.js
+++ dowell-survey/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    extensions: ['.mjs', '.js', '.jsx', '.json'],
+  },
   plugins: [react()],
-  base: '/dowell-survey/',
+  base: '/DoWell-Survey/dowell-survey/',
 })

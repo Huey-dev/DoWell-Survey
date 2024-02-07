0f4e4e1579663904ec214b3cd040147acecb2c0c
ijerry-cloud <jerrychinedu65@gmail.com>
2024-02-07 11:12:41 +0100

    rearranged the table on edit page to make actions visible

--- src/Pages/EditSurvey.jsx
+++ src/Pages/EditSurvey.jsx
@@ -29,9 +29,9 @@ export default function Edit() {
     const center = [9.055625, 7.480715];
 
     //for the maps rendering 
-    const [mapBounds, setMapBounds] = useState(null);
+    const [mapBounds, setMapBounds] = useState([[-90, -180], [90, 180]]);
     //setMapBounds([[32.6455, 56.55], [11.23, 53.442]])
-    const [locations, setLocations] = useState([])
+    const [locations, setLocations] = useState(surveys[0].places)
     const [map, setMap] = useState(null)
 
     function getCurrentDate() {
@@ -124,7 +124,7 @@ export default function Edit() {
             setMapBounds(bounds);
             setLocations(places)
             map.fitBounds(bounds)
-            window.scroll(0, 0);
+            //window.scroll(0, 0);
 
 
         }
@@ -417,21 +417,12 @@ export default function Edit() {
                                     }
 
                                 </MapContainer>
-
                             </div>
-
-
-
                         </div>
-
-
-
-
-
                     </div>
 
                     <div class="flex flex-col">
-                        <div class="overflow-x-auto ">
+                        <div class="overflow-x-auto">
                             <div class="inline-block py-2">
                                 <div class="overflow-hidden">
                                     <table class="min-w-full text-center text-sm font-light">
@@ -441,9 +432,12 @@ export default function Edit() {
                                                 <th scope="col" class=" px-6 py-4">BRAND NAME</th>
                                                 <th scope="col" class=" px-6 py-4">SURVEY LINK</th>
                                                 <th scope="col" class=" px-6 py-4">DURATION</th>
-                                                <th scope="col" class=" px-6 py-4">REGION</th>
-                                                <th scope="col" class=" px-6 py-4">STATUS</th>
                                                 <th scope="col" class=" px-6 py-4">ACTIONS</th>
+                                                <th scope="col" class=" px-6 py-4">STATUS</th>
+                                                <th scope="col" class=" px-6 py-4">REGION</th>
+                                                
+                                                
+                                                
                                             </tr>
                                         </thead>
                                         <tbody>
@@ -452,14 +446,7 @@ export default function Edit() {
                                                     <td class="whitespace-nowrap  px-6 py-4 font-medium bg-[#F3F6FF]">{survey.brand_name}</td>
                                                     <td class="whitespace-nowrap  px-6 py-4">{survey.link}</td>
                                                     <td class="whitespace-nowrap  px-6 py-4 bg-[#F3F6FF]">{`${survey.start_date} to ${survey.end_date} (1 day duration)`}</td>
-                                                    <td class="whitespace-nowrap  px-6 py-4">3 places(Click marker to view)</td>
-
-                                                    <td class="whitespace-nowrap bg-[#F3F6FF] font-medium">
-                                                        <div className="mx-4 my-2 bg-[#EF4444] text-white">
-                                                            ENDED
-                                                        </div></td>
-
-
+                                                    
                                                     <td class="whitespace-nowrap  px-6 py-4">
                                                         <div class="flex items-center justify-center space-x-0.5">
                                                             <button className="flex items-center justify-center rounded-lg bg-[#005734]"
@@ -482,6 +469,15 @@ export default function Edit() {
 
 
                                                     </td>
+                                                    <td class="whitespace-nowrap bg-[#F3F6FF] font-medium">
+                                                        <div className="mx-4 my-2 bg-[#EF4444] text-white">
+                                                            ENDED
+                                                        </div></td>
+                                                    <td class="whitespace-nowrap  px-6 py-4">3 places(Click marker to view)</td>
+
+                                                    
+
+
                                                 </tr>
 
                                             ))}
